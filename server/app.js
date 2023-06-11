e1a9cfad80cb7f3499a5355879e4f2daf6a7c4aa
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-06-11 11:53:03 +0200

    Server updated

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-require('dotenv').config();
-const { google } = require('googleapis');
-const nodemailer = require('nodemailer');
+const express = require("express");
+require("dotenv").config();
+const { google } = require("googleapis");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
@@ -10,65 +10,77 @@ const app = express();
 app.use(express.json());
 
 const myOAuth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-)
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
 
 myOAuth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
+  refresh_token: process.env.REFRESH_TOKEN,
 });
 
-const myAccessToken = myOAuth2Client.getAccessToken();
-
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 app.use(express.static(`${__dirname}/../front/build`));
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            type: 'OAuth2',
-            user: process.env.USER_MAIL,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: myAccessToken
-        },
-    })
+app.post("/", async (req, res) => {
+  console.log(req.body);
 
-    const mailOptions = {
-        from: {
-            name: req.body.name,
-            address: req.body.mail
-        },
-        to: 'gustin.samuel@gmail.com',
-        subject: req.body.subject,
-        text: req.body.text,
-        replyTo: req.body.mail
+  if (!name || !mail || !text) {
+    return res
+      .status(400)
+      .json({ error: "Les champs nom, email et message sont obligatoires." });
+  }
+
+  const myAccessToken = await myOAuth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      type: "OAuth2",
+      user: process.env.USER_MAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: myAccessToken,
+    },
+  });
+
+  const mailOptions = {
+    from: {
+      name: req.body.name,
+      address: req.body.mail,
+    },
+    to: "gustin.samuel@gmail.com",
+    subject: req.body.subject,
+    text: req.body.text,
+    replyTo: req.body.mail,
+  };
+
+  console.log(mailOptions);
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
     }
-
-    console.log(mailOptions)
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if(error){
-            console.log(error);
-            res.send('error');
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.send('success');
-        }
-    })
+  });
 });
 
 // app.listen(PORT, () => {
